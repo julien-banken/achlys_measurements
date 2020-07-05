00921306538406b9e33f20122bb88e0e79c7115e
Julien Banken <julien.banken@student.uclouvain.be>
2020-07-05 15:53:13 +0200

    EMA + Exponential decay
    Co-authored-by: Xanthos Nicolas <nicolas.xanthos@student.uclouvain.be>

--- EMA.py
+++ EMA.py
@@ -10,12 +10,16 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-def exp(x, param):
-  return param * math.exp(- param * x) if x >= 0 else 0
+# def generate_points():
+#   a = 0.1
+#   b = 3
+#   xs = np.linspace(10, 1)
+#   ys = [a * x + b for x in xs]
+#   return (xs, ys)
 
 def generate_points():
   xs = np.linspace(10, 1)
-  ys = np.sin(xs) + np.random.normal(scale=0.5, size=len(xs))
+  ys = np.sin(xs) + np.random.normal(scale=0.5, size=len(xs)) + 5
   return (xs, ys)
 
 def plot_data(ax, data):
@@ -24,74 +28,73 @@ def plot_data(ax, data):
   ax.set_xlabel("time")
   ax.set_ylabel("Response time")
 
-def plot_exponential_distribution(ax, param, color):
-  xs = range(10)
-  ys = [exp(x, param) for x in xs]
-  ax.set_title("Exponential distribution")
-  ax.scatter(xs, ys, color=color)
+def get_ema(data, t, params):
+  """
+  params = {
+    "tau": 1
+  }
+  """
+  average = 0
+  points = list(zip(*data))
+  for (x, y) in reversed(points):
+    if x > t:
+      return average
+    alpha = 1 - math.exp(- (t - x) / params["tau"])
+    average = average + alpha * (y - average)
+  return average
+
+def get_decay(point, t, params):
+  (x, y) = point
+  dt = t - x
+  return y - (y * math.exp(-params["lambda"] * dt))
+
+def get_last_point(data, t):
+  points = list(zip(*data))
+  last = None
+  for (x, y) in reversed(points):
+    if x > t:
+      return last
+    last = (x, y)
+  return last
+
+def plot_ema(ax, data, params, color):
+  xs = np.linspace(20, 1, num=2000)
+  ys = []
+  for x in xs:
+    ema = get_ema(data, x, params)
+    ys.append(ema)
   line, = ax.plot(xs, ys, color=color)
   return line
 
-def get_terms(data, t, param):
-
-  epsilon = 10**-3
-  terms = []
-
-  for (x, y) in zip(*data):
-    if x > t:
-      continue
-    else:
-      weight = exp(t - x, param)
-      if weight < epsilon:
-        return terms
-      terms.append((weight, y))
-
-  return terms
-
-def get_ema(data, t, param):
-
-  terms = get_terms(data, t, param)
-  weight_sum = 0
-
-  for (weight, y) in terms:
-    weight_sum = weight_sum + weight
-
-  average = 0
-  for (weight, y) in terms:
-    # average = average + (weight / weight_sum) * y
-    average = average + (weight) * y
-
-  return average
-
-def plot_ema(ax, data, param, color):
-  xs = np.linspace(20, 1, num=200)
+def plot_ema_with_decay(ax, data, params, color):
+  xs = np.linspace(20, 1, num=2000)
   ys = []
   for x in xs:
-    ys.append(get_ema(data, x, param))
+    last_point = get_last_point(data, x)
+    ema = get_ema(data, x, params)
+    decay = get_decay(last_point, x, params)
+    ys.append(ema - decay)
   line, = ax.plot(xs, ys, color=color)
   return line
 
 if __name__ == "__main__":
 
-  ncol = 2
+  fig = plt.figure(figsize=(9, 3), dpi=80)
+  fig.tight_layout()
+
+  ncol = 1
   nrow = 1
-  fig, axs = plt.subplots(nrow, ncol, figsize=(8, 4))
-  
+  grid = (nrow, ncol)
+
+  params = {
+    "tau": 1,
+    "lambda": 1
+  }
+
   data = generate_points()
-  plot_data(axs[0], data)
-  
-  params = [0.5, 1, 2]
-  colors = ["green", "orange", "red"]
-
-  a = []
-  b = []
-
-  for (param, color) in zip(params, colors):
-    a.append(plot_ema(axs[0], data, param, color))
-    b.append(plot_exponential_distribution(axs[1], param, color))
-
-  legends = ["λ = {0}".format(param) for param in params]
-  axs[0].legend(a, legends)
-  axs[1].legend(b, legends)
+  ax = plt.subplot2grid(grid, (0, 0))
+  plot_data(ax, data)
+  plot_ema(ax, data, params, "red")
+  plot_ema_with_decay(ax, data, params, "orange")
 
   plt.show()
